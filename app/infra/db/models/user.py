b752670c420c1d6aa3f73156db3313b332f545a5
Vadim Radzih <iphonevadim2003@gmail.com>
2023-09-08 12:09:52 +0300

    Setup project

---
+++ app/infra/db/models/user.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
+
+from .base import Base
+
+
+@dataclass
+class UserDTO:
+    id: int
+    name: str
+    created_time: datetime
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    created_time: Mapped[datetime] = mapped_column(default=func.now())
