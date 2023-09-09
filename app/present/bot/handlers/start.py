a634ec46d066b9cfec1da0e0ba4fcabfb07221b6
Vadim Radzih <iphonevadim2003@gmail.com>
2023-09-09 20:58:49 +0300

    Set commands on start

--- app/present/bot/handlers/start.py
+++ app/present/bot/handlers/start.py
@@ -1,4 +1,4 @@
-from aiogram import F, Router
+from aiogram import F, Router, Bot
 from aiogram.filters import CommandStart
 from aiogram.types import Message, User
 from aiogram.utils.keyboard import InlineKeyboardBuilder
@@ -7,13 +7,14 @@ from app.core.user import dto
 from app.core.user.interactor import CreateUser
 from app.present.bot import locales
 from app.present.bot.keyboards.inline import button
+from app.present.bot.services import commands
 
 router = Router()
 
 
 @router.message(CommandStart(), F.from_user.as_("user"))
 async def start_handler(
-    message: Message, user: User, create_user: CreateUser
+    message: Message, user: User, create_user: CreateUser, bot: Bot
 ) -> None:
     user_create = dto.UserCreate(id=user.id, name=user.full_name)
 
@@ -21,6 +22,8 @@ async def start_handler(
 
     builder = InlineKeyboardBuilder()
     builder.add(button.send_wallet_address)
+
+    await commands.set_commands(bot, user.id)
 
     await message.answer(
         locales.uk.START_MESSAGE.format(name=user.full_name),
